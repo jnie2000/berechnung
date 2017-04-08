8c160f5a31ace58442babf5fed6f2c5e5762a929
Juergen <j.niemeyer@me.com>
2017-04-08 19:01:16 +0200

    initial project version

---
+++ betriebBuilder.js
@@ -0,0 +1,321 @@
+// Version 1.2 - 28.03.2017
+// Variable für Nennweite und Kennwerte SR
+var kennSR = {
+    "kennSR": [{
+        "nennweiteSR": "DN15",
+        "mSR": 0.5806,
+        "kSR": 0.07066
+    }, {
+        "nennweiteSR": "DN20",
+        "mSR": 0.5880,
+        "kSR": 0.1273
+    }, {
+        "nennweiteSR": "DN25",
+        "mSR": 0.5908,
+        "kSR": 0.1779
+    }, {
+        "nennweiteSR": "DN32",
+        "mSR": 0.5885,
+        "kSR": 0.2940
+    }, {
+        "nennweiteSR": "DN40",
+        "mSR": 0.6107,
+        "kSR": 0.4203
+    }, {
+        "nennweiteSR": "DN50",
+        "mSR": 0.5806,
+        "kSR": 0.7066
+    }, {
+        "nennweiteSR": "DN65",
+        "mSR": 0.5876,
+        "kSR": 1.1651
+    }, {
+        "nennweiteSR": "DN80",
+        "mSR": 0.5880,
+        "kSR": 1.5525
+    }, {
+        "nennweiteSR": "DN100",
+        "mSR": 0.5804,
+        "kSR": 2.5728
+    }, {
+        "nennweiteSR": "DN125",
+        "mSR": 0.6309,
+        "kSR": 2.7359
+    }, {
+        "nennweiteSR": "DN150",
+        "mSR": 0.6301,
+        "kSR": 4.1246
+    }, {
+        "nennweiteSR": "DN200",
+        "mSR": 0.6332,
+        "kSR": 6.9619
+    } ]
+};
+
+// Variable für Nennweite und Kennwerte ZRK
+var kennZRK = {
+    "kennZRK": [{
+        "nennweiteZRK": "DN50",
+        "mZRK": 0.5113,
+        "kZRK": 0.8781
+    }, {
+        "nennweiteZRK": "DN65",
+        "mZRK": 0.4927,
+        "kZRK": 2.0679
+    }, {
+        "nennweiteZRK": "DN80",
+        "mZRK": 0.4942,
+        "kZRK": 2.2934
+    }, {
+        "nennweiteZRK": "DN100",
+        "mZRK": 0.5109,
+        "kZRK": 5.7044
+    }, {
+        "nennweiteZRK": "DN125",
+        "mZRK": 0.5183,
+        "kZRK": 13.1600
+    }, {
+        "nennweiteZRK": "DN150",
+        "mZRK": 0.4914,
+        "kZRK": 22.0500
+    }, {
+        "nennweiteZRK": "DN200",
+        "mZRK": 0.4927,
+        "kZRK": 29.0985
+    }, {
+        "nennweiteZRK": "DN250",
+        "mZRK": 0.4908,
+        "kZRK": 45.3834
+    }, {
+        "nennweiteZRK": "DN300",
+        "mZRK": 0.5109,
+        "kZRK": 57.0445
+    }, {
+        "nennweiteZRK": "DN350",
+        "mZRK": 0.5000,
+        "kZRK": 89.4427
+    }, {
+        "nennweiteZRK": "DN400",
+        "mZRK": 0.4900,
+        "kZRK": 124.67304
+    }, {
+        "nennweiteZRK": "DN450",
+        "mZRK": 0.4983,
+        "kZRK": 212.3720
+    }, {
+        "nennweiteZRK": "DN500",
+        "mZRK": 0.5048,
+        "kZRK": 310.6115
+    }, {
+        "nennweiteZRK": "DN600",
+        "mZRK": 0.4979,
+        "kZRK": 520.8001
+    }, {
+        "nennweiteZRK": "DN700",
+        "mZRK": 0.5099,
+        "kZRK": 802.0979
+    }, {
+        "nennweiteZRK": "DN800",
+        "mZRK": 0.5000,
+        "kZRK": 1414.2135
+    }, {
+        "nennweiteZRK": "DN900",
+        "mZRK": 0.5000,
+        "kZRK": 2236.0679
+    }, {
+        "nennweiteZRK": "DN1000",
+        "mZRK": 0.4927,
+        "kZRK": 2909.8516
+    }]
+};
+
+// Variable für Nennweite und Kennwerte ZRD
+var kennZRD = {
+    "kennZRD": [{
+        "nennweiteZRD": "DN50",
+        "mZRD": 0.5169,
+        "kZRD": 1.9536
+    }, {
+        "nennweiteZRD": "DN65",
+        "mZRD": 0.5028,
+        "kZRD": 3.3125
+    }, {
+        "nennweiteZRD": "DN80",
+        "mZRD": 0.5119,
+        "kZRD": 4.9180
+    }, {
+        "nennweiteZRD": "DN100",
+        "mZRD": 0.5181,
+        "kZRD": 7.9037
+    }, {
+        "nennweiteZRD": "DN125",
+        "mZRD": 0.5228,
+        "kZRD": 12.931
+    }, {
+        "nennweiteZRD": "DN150",
+        "mZRD": 0.5228,
+        "kZRD": 20.2276
+    }, {
+        "nennweiteZRD": "DN200",
+        "mZRD": 0.5181,
+        "kZRD": 39.5186
+    }, {
+        "nennweiteZRD": "DN250",
+        "mZRD": 0.5093,
+        "kZRD": 70.7355
+    }, {
+        "nennweiteZRD": "DN300",
+        "mZRD": 0.5113,
+        "kZRD": 110.9114
+    }, {
+        "nennweiteZRD": "DN350",
+        "mZRD": 0.5175,
+        "kZRD": 146.1115
+    }, {
+        "nennweiteZRD": "DN400",
+        "mZRD": 0.5195,
+        "kZRD": 189.7755
+    }, {
+        "nennweiteZRD": "DN450",
+        "mZRD": 0.5183,
+        "kZRD": 221.1066
+    }, {
+        "nennweiteZRD": "DN500",
+        "mZRD": 0.5178,
+        "kZRD": 320.5121
+    }, {
+        "nennweiteZRD": "DN600",
+        "mZRD": 0.5157,
+        "kZRD": 436.0509
+    }, {
+        "nennweiteZRD": "DN700",
+        "mZRD": 0.5113,
+        "kZRD": 616.0818
+    }, {
+        "nennweiteZRD": "DN800",
+        "mZRD": 0.5113,
+        "kZRD": 878.1781
+    }, {
+        "nennweiteZRD": "DN900",
+        "mZRD": 0.5113,
+        "kZRD": 1109.1142
+    }, {
+        "nennweiteZRD": "DN1000",
+        "mZRD": 0.5157,
+        "kZRD": 1329.9670
+    }, {
+        "nennweiteZRD": "DN1200",
+        "mZRD": 0.5157,
+        "kZRD": 1756.3562
+    }, {
+        "nennweiteZRD": "DN1400",
+        "mZRD": 0.5109,
+        "kZRD": 2281.7817
+    }, {
+		"nennweiteZRD": "DN1500",
+        "mZRD": 0.4747,
+        "kZRD": 3122.6790
+	}]
+};
+
+// Variable für die Typen
+var type = ["Rückschlagventil SR", "Doppelrückschlagklappe ZRD", "Rückschlagklappe ZRK"];
+
+// Variable für die Nennweiten
+var nennweite = ["DN15", "DN20", "DN25", "DN32", "DN40", "DN50", "DN65", "DN80", "DN100",
+    "DN125", "DN150", "DN200", "DN250", "DN300", "DN350", "DN400", "DN450", "DN500",
+    "DN600", "DN700", "DN800", "DN900", "DN1000", "DN1200", "DN1400", "DN1500"];
+
+// Auswahlliste Typ füllen
+function typen_liste() {
+    var selectType = document.getElementById("Type");
+    for (var i = 0; i < 3; i++) {
+        var opt = document.createElement("option");
+        selectType.appendChild(opt);
+        opt.innerHTML = type[i];
+        opt.value = type[i];
+    };
+}
+
+// Auswahlliste Nennweite füllen
+function nennweite_liste() {
+    selectNennweite = document.getElementById("Nennweite");
+    selectType = document.getElementById("Type").value;
+    if (selectType == "Rückschlagventil SR") {
+        var i = 0;
+        var j = 12;
+    } else {
+        var i = 5;
+        var j = nennweite.length;
+    }
+    selectNennweite.length = 0;
+    for (i; i < j; i++) {
+        var opt = document.createElement("option");
+        selectNennweite.appendChild(opt);
+        opt.innerHTML = nennweite[i];
+        opt.value = nennweite[i];
+
+    };
+}
+// Berechnung Wasservolumen und Druckverlust
+
+function wasservolumen() {
+    // Eingabewerte abfragen
+    var volumen = parseFloat(document.getElementById("Volumenstrom").value);
+    var dichte = parseFloat(document.getElementById("Betriebsdichte").value);
+
+    // Wasservolumenberechnen
+    var wvolumen = volumen * Math.sqrt(dichte / 1000);
+    wvolumen = wvolumen.toFixed(2);
+    // Kennwerte der gewaehlten Type und Nennweite ermitteln
+    var sType = document.getElementById("Type").value;
+    var iNennweite = document.getElementById("Nennweite").selectedIndex;
+
+    if (sType == "Rückschlagventil SR") {
+        var k = kennSR.kennSR[iNennweite].kSR;
+        var m = kennSR.kennSR[iNennweite].mSR;
+    };
+    if (sType == "Rückschlagklappe ZRK") {
+        var k = kennZRK.kennZRK[iNennweite].kZRK;
+        var m = kennZRK.kennZRK[iNennweite].mZRK;
+    };
+    if (sType == "Doppelrückschlagklappe ZRD") {
+        var k = kennZRD.kennZRD[iNennweite].kZRD;
+        var m = kennZRD.kennZRD[iNennweite].mZRD;
+    };
+    console.log(k);
+    console.log(m);
+    // Druckverlust berechnen
+    var exponent = (Math.log10(wvolumen) - Math.log10(k)) / m;
+    var dverlust = Math.pow(10, exponent);
+    dverlust = dverlust.toFixed(0);
+    wvolumen = wvolumen.toString();
+    wvolumen = wvolumen.replace(".", ",");
+
+    console.log(volumen);
+    console.log(wvolumen);
+    console.log(dverlust);
+    console.log(sType);
+    console.log(iNennweite);
+	console.log(kennZRD.kennZRD[iNennweite]);
+    var ergebnisVolumen = "<p><h2>Ergebnis der Berechnung</h2></p>" + "Der equivalente Wasservolumenstrom beträgt: " + wvolumen + " m&sup3;/h";
+    var ergebnisDruck = "Der Druckverlust beträgt: " + dverlust + " mbar.";
+
+    if (isNaN(volumen) == true) {
+        alert("Bitte Volumenstrom eingeben");
+    }
+    if (isNaN(dichte) == true) {
+        alert("Bitte Dichte eingeben");
+    } else {
+        document.getElementById("ErgebnisVolumen").innerHTML = ergebnisVolumen;
+
+    }
+
+    if (k == undefined) {
+        alert("Bitte Type und Nennweite wählen");
+    } else {
+        document.getElementById("ErgebnisDruck").innerHTML = ergebnisDruck;
+    }
+
+
+}
